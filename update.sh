105b0e2e6dce664b231bbb08895a5276744c38ff
hex <hexhex16@outlook.com>
2021-08-01 16:32:22 +0800

    ctf: pwn

--- update.sh
+++ update.sh
@@ -8,7 +8,7 @@ var=$1
 # echo $var
 
 echo "[Step-1] git pull origin main:"
-git pull origin main -v;
+git pull origin master -v;
 echo
 echo "=== [WARNING] If conflict occured, related files will show below: ========="
 git status -v
@@ -16,11 +16,11 @@ echo "=== [WARNING] Fix by hands if conflict exits. Then [RE-RUN] this script ==
 echo 
 
 echo "[Step-2] Local changes add and commit:"
-git checkout main -v;
+git checkout master -v;
 git add . -v;
 
 git commit -v -am "$var";
 
 
 echo "[Step-3] git push origin main:"
-git push origin main -v
+git push origin master -v
