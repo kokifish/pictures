37fbd57c458d928c79f21a2ce46c0c10ce97fa7d
hex <hexhex16@outlook.com>
2021-08-01 16:31:05 +0800

    ctf: pics

--- update.sh
+++ update.sh
@@ -1,5 +1,26 @@
-git add .;
-var=`date "+%Y-%m-%d_%H:%M:%S"`
-echo $var
-git commit -m $var;
-git push origin master
+if [ ! -n "$1" ];then
+    echo "please give commit massge after bash. e.g. 
+    > bash update.sh \"code: virtual table pics\"
+    > bash update.sh \"ctf: pwn stack map\""
+    exit 1
+fi
+var=$1
+# echo $var
+
+echo "[Step-1] git pull origin main:"
+git pull origin main -v;
+echo
+echo "=== [WARNING] If conflict occured, related files will show below: ========="
+git status -v
+echo "=== [WARNING] Fix by hands if conflict exits. Then [RE-RUN] this script ==="
+echo 
+
+echo "[Step-2] Local changes add and commit:"
+git checkout main -v;
+git add . -v;
+
+git commit -v -am "$var";
+
+
+echo "[Step-3] git push origin main:"
+git push origin main -v
